ffacf9ca98b8a66c836a91236100aa68d11e965f
Hudson Ferreira Leite <mestihudson@gmail.com>
2023-10-18 10:15:55 -0300

    chore: add config to run playwright tests

---
+++ opinion-ate-2/playwright.config.ts
@@ -0,0 +1,39 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: 'e2e',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: [
+    ['html', { open: 'never', }, ],
+    ['list', { printSteps: true, }, ],
+  ],
+  use: {
+    actionTimeout: 0,
+    baseURL: process.env.E2E_BASE_URL || 'http://dev:4200',
+    trace: 'on-first-retry',
+    video: { mode: 'on', },
+    screenshot: { mode: 'on', fullPage: true, },
+    launchOptions: {
+      slowMo: parseInt(process.env.E2E_SLOW_MO || 0),
+    },
+  },
+  testMatch: process.env.E2E_TEST_MATCH || '**/*.e2e-spec.ts',
+  grep: new RegExp(process.env.E2E_GREP || ''),
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+  ],
+});
