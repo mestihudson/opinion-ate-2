d8e2ee422f06040be383fd1678589357bd431823
Hudson Ferreira Leite <mestihudson@gmail.com>
2023-10-27 17:13:44 +0000

    chore: add jest configs to tests see by alias paths

--- opinion-ate-2/jest.config.js
+++ opinion-ate-2/jest.config.js
@@ -1,4 +1,12 @@
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig');
+
 module.exports = {
   preset: 'jest-preset-angular',
+  roots: ['<rootDir>/src/'],
+  testMatch: ['**/+(*.)+(spec).+(ts)'],
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths || {}, {
+    prefix: '<rootDir>/',
+  }),
 };
