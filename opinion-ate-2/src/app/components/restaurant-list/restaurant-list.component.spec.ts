03687384a8ff795c9ef296ab1a7fc3927d4558e6
Hudson Ferreira Leite <mestihudson@gmail.com>
2023-10-29 14:06:07 +0000

    refactor(test): remove duplication

--- opinion-ate-2/src/app/components/restaurant-list/restaurant-list.component.spec.ts
+++ opinion-ate-2/src/app/components/restaurant-list/restaurant-list.component.spec.ts
@@ -1,6 +1,6 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { of } from 'rxjs';
+import { of, Observable } from 'rxjs';
 
 import { RestaurantListComponent } from '@/app/components/restaurant-list/restaurant-list.component';
 import { RestaurantListService } from '@/app/services/restaurant-list.service';
@@ -20,6 +20,13 @@ describe('RestaurantListComponent', () => {
     fixture.detectChanges();
     return { fixture, component };
   };
+  const mockRestaurantListService = {
+    get: (): Observable<{ id: number; name: string }[]> => of([]),
+  };
+  const provider = {
+    provide: RestaurantListService,
+    useValue: mockRestaurantListService,
+  };
 
   it('should render without errors', async () => {
     const { component } = await renderComponent();
@@ -27,15 +34,8 @@ describe('RestaurantListComponent', () => {
   });
 
   it('should load restaurants on first render', async () => {
-    const mockRestaurantListService = {
-      get: jest.fn(() => of([])),
-    };
-    await renderComponent([
-      {
-        provide: RestaurantListService,
-        useValue: mockRestaurantListService,
-      },
-    ]);
+    mockRestaurantListService.get = jest.fn(() => of([]));
+    await renderComponent([provider]);
     expect(mockRestaurantListService.get).toHaveBeenCalled();
   });
 
@@ -44,15 +44,8 @@ describe('RestaurantListComponent', () => {
       { id: 1, name: 'Sushi Place' },
       { id: 2, name: 'Pizza Place' },
     ];
-    const mockRestaurantListService = {
-      get: jest.fn(() => of([...restaurants])),
-    };
-    const { fixture } = await renderComponent([
-      {
-        provide: RestaurantListService,
-        useValue: mockRestaurantListService,
-      },
-    ]);
+    mockRestaurantListService.get = jest.fn(() => of([...restaurants]));
+    const { fixture } = await renderComponent([provider]);
     expect(fixture.debugElement.nativeElement.textContent).toEqual(
       expect.stringMatching(/(Sushi Place|Pizza Place)/),
     );
