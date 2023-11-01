bc45c4c0e9d897ad292c6fbbb2ee5e1a75685cfa
Hudson Ferreira Leite <mestihudson@gmail.com>
2023-11-01 18:31:08 +0000

    refactor: remove extends to prove a point

--- opinion-ate-2/src/app/adapters/load-restaurants-http.adapter.ts
+++ opinion-ate-2/src/app/adapters/load-restaurants-http.adapter.ts
@@ -2,15 +2,12 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
-import { LoadRestaurantsInteractor } from '@/app/interactors/load-restaurants.interactor';
 import { RestaurantModel } from '@/app/models/restaurant.model';
 import { environment } from '@/environments/environment';
 
 @Injectable({ providedIn: 'root' })
-export class LoadRestaurantsHttpAdapter extends LoadRestaurantsInteractor {
-  constructor(private http: HttpClient) {
-    super();
-  }
+export class LoadRestaurantsHttpAdapter {
+  constructor(private http: HttpClient) {}
 
   get(): Observable<RestaurantModel[]> {
     const token = 'API_HASH_TOKEN';
