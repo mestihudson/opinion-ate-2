74a7b5bc3fcaf4900858f3a989bf1c8a6b23a365
Hudson Ferreira Leite <mestihudson@gmail.com>
2023-11-01 15:36:53 -0300

    Revert "refactor: remove extends to prove a point"
    This reverts commit bc45c4c0e9d897ad292c6fbbb2ee5e1a75685cfa.

--- opinion-ate-2/src/app/adapters/load-restaurants-http.adapter.ts
+++ opinion-ate-2/src/app/adapters/load-restaurants-http.adapter.ts
@@ -2,12 +2,15 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
+import { LoadRestaurantsInteractor } from '@/app/interactors/load-restaurants.interactor';
 import { RestaurantModel } from '@/app/models/restaurant.model';
 import { environment } from '@/environments/environment';
 
 @Injectable({ providedIn: 'root' })
-export class LoadRestaurantsHttpAdapter {
-  constructor(private http: HttpClient) {}
+export class LoadRestaurantsHttpAdapter extends LoadRestaurantsInteractor {
+  constructor(private http: HttpClient) {
+    super();
+  }
 
   get(): Observable<RestaurantModel[]> {
     const token = 'API_HASH_TOKEN';
