e6684f1e3f3c91a1db90305959fa5ddb85962a7e
Hudson Ferreira Leite <mestihudson@gmail.com>
2023-11-14 09:56:43 +0000

    test: should create an action to load restaurants successfully

--- opinion-ate-2/src/app/store/restaurant.actions.spec.ts
+++ opinion-ate-2/src/app/store/restaurant.actions.spec.ts
@@ -1,12 +1,27 @@
+import { RestaurantModel } from '@/app/models/restaurant.model';
 import * as fromRestaurants from '@/app/store';
 
-describe('Restaurant Actions', () => {
+describe('RestaurantActions', () => {
   describe('loadRestaurants', () => {
     it('should create an action to load restaurants', () => {
       const expected = {
         type: fromRestaurants.loadRestaurants.type,
       };
       const action = fromRestaurants.loadRestaurants();
+      expect(action).toEqual(expected);
+    });
+  });
+
+  describe('loadRestaurantsSuccess', () => {
+    it('should create an action to load restaurants successfully', () => {
+      const mockedRestaurantList: RestaurantModel[] = [];
+      const expected = {
+        type: fromRestaurants.loadRestaurantsSuccess.type,
+        restaurants: mockedRestaurantList,
+      };
+      const action = fromRestaurants.loadRestaurantsSuccess({
+        restaurants: mockedRestaurantList,
+      });
       expect(action).toEqual(expected);
     });
   });
