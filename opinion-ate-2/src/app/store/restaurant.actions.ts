2a47126eb268620c65e023f0219e0b9351bd4fdf
Hudson Ferreira Leite <mestihudson@gmail.com>
2023-11-14 09:58:00 +0000

    feat: create an action to load restaurants successfully

--- opinion-ate-2/src/app/store/restaurant.actions.ts
+++ opinion-ate-2/src/app/store/restaurant.actions.ts
@@ -1,5 +1,14 @@
-import { createAction } from '@ngrx/store';
+import { createAction, props } from '@ngrx/store';
+
+import { RestaurantModel } from '@/app/models/restaurant.model';
 
 const prefix = '[Restaurant]';
 
 export const loadRestaurants = createAction(`${prefix} Load restaurants`);
+
+export const loadRestaurantsSuccess = createAction(
+  `${loadRestaurants.type} Success`,
+  props<{
+    restaurants: RestaurantModel[];
+  }>(),
+);
